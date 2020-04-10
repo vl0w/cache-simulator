3eee851627c5f4a696d66650accec593d154ad69
Jonas Hansen <ganymed92@gmail.com>
2020-04-10 10:55:58 +0200

    add type annotations

--- cachesimulator.py
+++ cachesimulator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import List, Dict, Optional
 
 ADDRESS_LENGTH_BITS = 64
 
@@ -15,11 +16,14 @@ def _bit_str_to_number(bit_string):
 
 
 class Variable:
-    def __init__(self, address: str):
-        self.address = address
+    _address: str
 
-    def get_address(self) -> str:
-        return self.address
+    def __init__(self, address: str):
+        self._address = address
+
+    @property
+    def address(self) -> str:
+        return self._address
 
     def simulate_read(self, cache):
         if cache.is_in_cache(self):
@@ -34,29 +38,49 @@ class Variable:
 
 
 class CacheLine:
-    def __init__(self):
-        self.tag = None
-        self.timestamp = -1
+    _tag: Optional[str]
+    _timestamp: int
 
-    def is_empty(self):
+    def __init__(self):
+        self._tag = None
+        self._timestamp = -1
+
+    @property
+    def tag(self) -> Optional[str]:
+        return self._tag
+
+    @property
+    def timestamp(self) -> int:
+        return self._timestamp
+
+    def is_empty(self) -> bool:
         return self.tag is None
 
-    def is_filled(self):
+    def is_filled(self) -> bool:
         return not self.is_empty()
 
     def is_older_than(self, other):
-        assert (self.timestamp != -1)
+        assert (self._timestamp != -1)
         assert (other.timestamp != -1)
-        return self.timestamp < other.timestamp
+        return self._timestamp < other.timestamp
 
     def put(self, timestamp, tag):
-        self.tag = tag
-        self.timestamp = timestamp
+        self._tag = tag
+        self._timestamp = timestamp
 
 
 class Cache:
+    _b: int
+    _e: int
+    _s: int
+    _B: int
+    _E: int
+    _S: int
+    _timestamp: int
+    _lines: List[List[CacheLine]]
+    _stats: Dict[str, int]
 
-    def __init__(self, total_size, block_size, associativity=1):
+    def __init__(self, total_size: int, block_size: int, associativity=1):
         b_float = np.log2(block_size)
         e_float = np.log2(associativity)
         if not b_float.is_integer():
@@ -66,31 +90,35 @@ class Cache:
         if not np.log2(total_size).is_integer():
             raise AttributeError("total cache size needs to be exponential with base 2")
 
-        self.b = int(b_float)
-        self.e = int(e_float)
-        self.B = int(block_size)
-        self.E = int(associativity)
-        self.S = int(total_size / block_size / associativity)
-        self.s = int(np.log2(self.S))
-        self.counter = 0  # for age of cache lines
-        self.stats = {
+        self._b = int(b_float)
+        self._e = int(e_float)
+        self._B = int(block_size)
+        self._E = int(associativity)
+        self._S = int(total_size / block_size / associativity)
+        self._s = int(np.log2(self._S))
+        self._timestamp = 0  # for age of cache lines
+        self._stats = {
             "hits": 0,
             "misses": 0,
             "accesses": 0
         }
 
-        self.lines = []
-        for _ in range(self.S):
+        self._lines = []
+        for _ in range(self._S):
             set_lines = []
-            for _ in range(self.E):
+            for _ in range(self._E):
                 set_lines.append(CacheLine())
-            self.lines.append(set_lines)
+            self._lines.append(set_lines)
+
+    @property
+    def stats(self) -> Dict[str, int]:
+        return self._stats
 
     def get_offset_bits(self, var: Variable) -> str:
-        return var.get_address()[-self.b:]
+        return var.address[-self._b:]
 
     def get_set_bits(self, var: Variable) -> str:
-        return var.get_address()[-self.b - self.s: -self.b]
+        return var.address[-self._b - self._s: -self._b]
 
     def get_set_index(self, var: Variable) -> int:
         set_bits = self.get_set_bits(var)
@@ -99,89 +127,89 @@ class Cache:
         else:
             return _bit_str_to_number(set_bits)
 
-    def get_tag_bits(self, var: Variable):
-        return var.get_address()[0:-self.b - self.s]
+    def get_tag_bits(self, var: Variable) -> str:
+        return var.address[0:-self._b - self._s]
 
     def is_in_cache(self, var: Variable) -> bool:
-        return self.__get_line(var) is not None
+        return self._get_line(var) is not None
 
-    def __is_set_full(self, set):
-        for line in self.lines[set]:
+    def __is_set_full(self, set_index: int):
+        for line in self._lines[set_index]:
             if line.is_empty():
                 return False
         return True
 
-    def __get_line(self, var):
+    def _get_line(self, var) -> Optional[CacheLine]:
         tag_bits = self.get_tag_bits(var)
-        set = self.get_set_index(var)
+        set_index = self.get_set_index(var)
 
-        for line in self.lines[set]:
+        for line in self._lines[set_index]:
             if line.is_filled() and line.tag == tag_bits:
                 return line
 
         return None
 
-    def __get_lru_line(self, set):
+    def _get_lru_line(self, set_index: int):
         lru_line = None
-        curr_min = self.counter
-        for l in self.lines[set]:
-            if l.timestamp < curr_min:
-                curr_min = l.timestamp
-                lru_line = l
+        curr_min = self._timestamp
+        for line in self._lines[set_index]:
+            if line.timestamp < curr_min:
+                curr_min = line.timestamp
+                lru_line = line
 
         assert (lru_line is not None)
         return lru_line
 
     def put_to_cache(self, var: Variable):
-        self.counter += 1
+        self._timestamp += 1
         if self.is_in_cache(var):
-            line = self.__get_line(var)
-            line.timestamp = self.counter
+            line = self._get_line(var)
+            line._timestamp = self._timestamp
             return
 
         tag_bits = self.get_tag_bits(var)
-        set = self.get_set_index(var)
+        set_index = self.get_set_index(var)
 
         cached = False
-        for line in self.lines[set]:
+        for line in self._lines[set_index]:
             if line.is_empty():
-                line.put(self.counter, tag_bits)
+                line.put(self._timestamp, tag_bits)
                 cached = True
                 break
 
         if cached:
             return
 
-        lru_line = self.__get_lru_line(set)
-        lru_line.put(self.counter, tag_bits)
+        lru_line = self._get_lru_line(set_index)
+        lru_line.put(self._timestamp, tag_bits)
 
     def print_var_info(self, var: Variable):
         offset_bits = self.get_offset_bits(var)
         set_bits = self.get_set_bits(var)
         tag_bits = self.get_tag_bits(var)
 
-        print("Variable: " + var.get_address())
+        print("Variable: " + var.address)
         print("Offset bits: {}, Offset: {}".format(offset_bits, _bit_str_to_number(offset_bits)))
         print("Tag bits: {}, Tag: {}".format(tag_bits, _bit_str_to_number(tag_bits)))
         print("Set bits: {}, Set: {}".format(set_bits, _bit_str_to_number(set_bits)))
 
-    def get_total_size(self):
-        return self.S * self.E * self.B
+    def get_total_size(self) -> int:
+        return self._S * self._E * self._B
 
 
-def create_variables(start_address_int: int, amount: int, size_per_var: int):
-    vars = []
+def create_variables(start_address_int: int, amount: int, size_per_var: int) -> List[Variable]:
+    variables: List[Variable] = []
     curr_address_int = start_address_int
     for _ in range(amount):
         curr_address_string = _number_to_bit_str(curr_address_int, ADDRESS_LENGTH_BITS)
-        vars.append(Variable(curr_address_string))
+        variables.append(Variable(curr_address_string))
         curr_address_int += size_per_var
-    return vars
+    return variables
 
 
-def create_floats(start_address_int: int, amount: int):
+def create_floats(start_address_int: int, amount: int) -> List[Variable]:
     return create_variables(start_address_int, amount, 4)
 
 
-def create_doubles(start_address_int: int, amount: int):
+def create_doubles(start_address_int: int, amount: int) -> List[Variable]:
     return create_variables(start_address_int, amount, 8)
--- examples/stridedaccess.py
+++ examples/stridedaccess.py
@@ -3,12 +3,12 @@ from cachesimulator import *
 n = 40
 stride = 16
 
-vars = create_doubles(0, n * stride)
-cache = Cache(4096., 64., 2.)
+variables = create_doubles(0, n * stride)
+cache = Cache(4096, 64, 2.)
 
 for j in range(2):
     for i in range(n):
-        vars[i * stride].simulate_read(cache)
+        variables[i * stride].simulate_read(cache)
 
 miss_rate = cache.stats["misses"] / cache.stats["accesses"]
 print(miss_rate)
